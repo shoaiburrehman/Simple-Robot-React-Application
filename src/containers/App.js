2c13d81b348f9b0c252324a249956782b05515ab
shoaiburrehman <shoaiburrehman@gmail.com>
2019-10-02 23:54:17 +0500

    my first commit

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll'
+import './App.css';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state={
+            robots: [],
+            searchfield: ''        
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render(){
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !robots.length ?                                         //Ternary Operator for if else
+            <h1 className='center'> LOADING... </h1>:
+        (
+            <div className='tc'>
+                <div>
+                <h1 className='f2'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                </div>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );
+    }
+}
+
+export default App;
